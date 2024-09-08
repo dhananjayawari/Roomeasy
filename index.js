46d0b41fc2d0336897902b985594ba624bdc7430
shadow-monarch-001 <vaidyakapilb@gmail.com>
2024-09-08 16:31:16 +0530

    cors change

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ dotenv.config();
 // Setting up port number
 const PORT = process.env.PORT || 4000;
 const corsOptions = {
-	origin: 'https://roomeasy-3lbj.onrender.com', // Your frontend's origin
+	origin: 'https://localhost:3000', // Your frontend's origin
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization']
   };
