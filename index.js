3c17b6cfd13eba4bdf239013704d2816b10a0a23
Dhananjay Awari <115948933+dhananjayawari@users.noreply.github.com>
2024-09-09 17:44:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ dotenv.config();
 // Setting up port number
 const PORT = process.env.PORT || 4000;
 const corsOptions = {
-	origin: 'https://66dee32819010dd6b7662d79--strong-strudel-ca76f5.netlify.app', // Your frontend's origin
+	origin: 'https://www.roomeasy.net', // Your frontend's origin
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization']
   };
